2f98d18e0a77a602035d3f09084f990071db8651
chasethesky79 <seshadri.bharath@yahoo.com>
2025-01-11 16:38:53 -0600

    Implemented OIDC authentication

--- src/main/java/com/polarbookshop/edge_service/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edge_service/config/RateLimiterConfig.java
@@ -4,10 +4,15 @@ import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @Configuration
 public class RateLimiterConfig {
 
     public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
+
     }
 }
